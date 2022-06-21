a562f8ceb8f3e9aabdad87a63e57e674477ed6ee
= <=>
2022-06-21 17:27:38 -0500

    vig fixes, added normalizations

--- Graph NNs/ViG/models.py
+++ Graph NNs/ViG/models.py
@@ -6,18 +6,23 @@ from modules import *
 
 
 class IsotropicVIG(nn.Module):
-    def __init__(self, in_channels, num_blocks, kernel_size=16):
+    def __init__(self, in_channels, num_blocks, patch_size=4, kernel_size=16):
         super().__init__()
 
         self.num_blocks = num_blocks
+        self.patch_size = patch_size
 
         self.patchifier = Patchifier(
-            patch_size=4, hidden_channels=in_channels, norm=True
+            patch_size=self.patch_size, hidden_channels=in_channels
         )
+
         self.network = nn.ModuleList()
+        self.norms = nn.ModuleList()
 
         for i in range(num_blocks):
             self.network.append(Block(in_channels, kernel_size))
+            if i < num_blocks-1:
+                self.norms.append(nn.BatchNorm2d(in_channels))
 
         self.reset_parameters()
 
@@ -30,6 +35,7 @@ class IsotropicVIG(nn.Module):
 
             if i < self.num_blocks - 1:
                 x = F.gelu(x)
+                x = self.norms[i](x)
 
         return x
 
@@ -47,7 +53,10 @@ class IsotropicVIG(nn.Module):
 
 def test_iso_vig():
     # Tiny config
-    model = IsotropicVIG(in_channels=192, num_blocks=12, kernel_size=16)
+    model = IsotropicVIG(in_channels=192, num_blocks=1, patch_size=4, kernel_size=16)
+
+    from torchsummary import summary
+    summary(model, (3, 224, 224))
 
     x = torch.randn(1, 3, 224, 224)
     print(model(x).shape)  # torch.Size([`batch_size`, `in_channels`, 56, 56])
